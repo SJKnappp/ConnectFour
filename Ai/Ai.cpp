474355535ad0c8b486ad1dfef06e3fba5a420316
Samuel Knapp <samuel.j.knapp@btinternet.com>
2020-12-13 18:02:33 +0000

    add Ai namespace

---
+++ Ai/Ai.cpp
@@ -0,0 +1,5 @@
+//
+// Created by knapp on 13/12/2020.
+//
+
+#include "Ai.h"
---
+++ Ai/Ai.h
@@ -0,0 +1,12 @@
+//
+// Created by knapp on 13/12/2020.
+//
+
+#ifndef CONNECTFOUR_AI_H
+#define CONNECTFOUR_AI_H
+
+namespace Ai {
+int minMax() {}
+}; // namespace Ai
+
+#endif // CONNECTFOUR_AI_H
